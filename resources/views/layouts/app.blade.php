a12912d18168cc17bd73db4b6ae51ee01d825997
SayidAchmad <syaidahmad7z@gmail.com>
2023-12-09 18:00:29 +0700

    Mengatur Template SB Admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,14 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        @auth
+                            <li class="nav-item">
+                                <a href="/home" class="nav-link">Beranda</a>    
+                            </li>
+                            <li class="nav-item">
+                                <a href="/admin" class="nav-link">Data Admin</a>    
+                            </li>
+                        @endauth
                     </ul>
 
                     <!-- Right Side Of Navbar -->
---
+++ resources/views/user_index.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.sbadmin2')
+@section('content')
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    {{ $judul }}
+                </div>
+                <div class="card-body">
+                    {{-- <a href="{{ route('dokter.create')}}" class="btn btn-primary mb-3">Tambah Dokter</a>
+                    <a href="{{ route('dokter.laporan')}}" class="btn btn-primary mb-3">Laporan Dokter</a> --}}
+                    <table class="table table-bordered table-hover table-striped">
+                        <thead>
+                            <td>ID</td>
+                            <td>Nama Pengguna</td>
+                            <td>Email</td>
+                            <td>Create</td>
+                            <td>Aksi</td>
+                        </thead>
+                        <tbody>
+                            @foreach ($user as $item)
+                                <tr>
+                                    <td>{{ $item->id }}</td>
+                                    <td>{{ $item->name }}</td>
+                                    <td>{{ $item->email }}</td>
+                                    <td>{{ $item->created_at }}</td>
+                                    {{-- <td class="d-flex justify-content-around">
+                                        <a href="{{ route('dokter.edit', $item->id)}}" class="btn btn-primary">Edit</a>
+                                        {!! Form::open([
+                                            'route' => ['dokter.destroy', $item->id],
+                                            'method' => 'delete',
+                                            'onsubmit' => 'return confirm("Yakin mau dihapus?")',
+                                        ]) !!}
+                                        {!! Form::submit('Hapus', ['class' => 'btn btn-danger']) !!}
+                                        {!! Form::close()!!}
+                                    </td> --}}
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
